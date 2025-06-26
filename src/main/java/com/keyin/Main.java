ddf4751b19518a1d812f1ce847ba2655457916e2
NyuSD <161166895+NyuSD@users.noreply.github.com>
2025-06-26 15:01:45 -0230

    Add user cli demo

--- src/main/java/com/keyin/Main.java
+++ src/main/java/com/keyin/Main.java
@@ -1,7 +1,30 @@
 package com.keyin;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        User[] users = new User[3];
+        users[0] = new User("Jacob");
+        users[1] = new User("Sandy");
+        users[2] = new User("Maple");
+
+
+        users[0].addTask("Pay bills");
+        users[0].addTask("Buy groceries");
+
+        users[1].addTask("Exercise");
+        users[1].addTask("Read a book");
+
+        users[2].addTask("Practice coding");
+
+
+        users[0].markTaskCompleted(1);
+        users[2].markTaskCompleted(0);
+
+
+        for (User u : users) {
+            u.printTasks();
+        }
     }
 }
